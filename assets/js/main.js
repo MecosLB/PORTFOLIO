a4f42ed98c25df01688c19957a76da8a24da4778
MecosLB <64880524+MecosLB@users.noreply.github.com>
2024-01-09 14:22:55 -0600

    Github web
    First deploy v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(() => {
+    const btnWhats = document.querySelector('#introduction .btn-chat'),
+        photo = document.querySelector('#introduction .photo');
+
+    document.addEventListener('DOMContentLoaded', e => {
+        btnWhats.addEventListener('mouseover', e => {
+            photo.src = '/assets/img/photo_hover.webp';
+        });
+        
+        btnWhats.addEventListener('mouseleave', e => {
+            photo.src = '/assets/img/photo_normal.webp';
+        });
+    });
+})();
